a39226d8aef12206c4f9dc77e3643879caecf733
Gordon Chan <me@gordon-chan.net>
2020-06-07 22:16:36 +0800

    Adopt environment variables

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -1,7 +1,7 @@
 const paypal = require('@paypal/checkout-server-sdk');
 
-let clientId = "AbH1wK350KsM50wu8XfzvbnN2B3Sn4PjCzzJGEA3-wIaG7zSbgNgyphVPdW0p1luiJR1XRr5yCQmeL0S";
-let clientSecret = "EAmLMHHfryzaeiOQZ1WiB6Kq6AKwZxX_SvzwFYGzT0h5ql3Q9KAsyxn0kQZgS57Ok7kJEngcPA1gzMwV";
+let clientId = process.env.paypalClientId;
+let clientSecret = process.env.paypalClientSecret;
 let environment = new paypal.core.SandboxEnvironment(clientId, clientSecret);
 let client = new paypal.core.PayPalHttpClient(environment);
 
