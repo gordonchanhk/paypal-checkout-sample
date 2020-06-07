20d21e8dbeb0f6776711b906540f51c6cf0b62b0
Gordon Chan <me@gordon-chan.net>
2020-06-07 21:00:00 +0800

    Setup product/order data

--- public/javascripts/checkout.js
+++ public/javascripts/checkout.js
@@ -1,3 +1,38 @@
+// Paypal button setup
+
+var checkoutConfig = {
+  shipping: {
+    address1: '5 Temasek Boulevard',
+    address2: '#09-01 Suntec Tower Five',
+    postalCode: '038985',
+    countryName: 'Singapore',
+    countryCode: 'SG'
+  },
+  items: [
+    {
+      name: 'EBook: The PayPal Wars v1',
+      description: 'Battles with eBay, the Media, the Mafia, and the Rest of Planet Earth',
+      price: '65.00',
+      currency: 'USD',
+      quantity: '1',
+      sku: 'sku001'
+    },
+    {
+      name: 'EBook: The PayPal Wars v2',
+      description: 'Battles with eBay, the Media, the Mafia, and the Rest of Planet Earth',
+      price: '61.00',
+      currency: 'USD',
+      quantity: '1',
+      sku: 'sku002'
+    }
+  ],
+  order: {
+    total: '65.00',
+    currency: 'USD'
+  },
+  completeUrl: './complete'
+};
+
 (function($) {
 
   function init() {
