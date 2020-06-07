a36218b572a6f02ac00a30ba543e7c07dbab6923
Gordon Chan <me@gordon-chan.net>
2020-06-07 20:52:11 +0800

    Setup routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var transactionCtlr = require('../controllers/transaction.js');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Checkout' });
 });
+
+/* GET Checkout complete. */
+router.post('/complete', function(req, res, next) {
+  res.render('complete', { txnId: req.body.txnId });
+});
+
+/* Handle order capturing */
+router.post('/checkout/capture', transactionCtlr.capture);
 
 module.exports = router;
