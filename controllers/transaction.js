1fa55a36d97ec901481b2cf3967d6a977c01d74a
Gordon Chan <me@gordon-chan.net>
2020-06-07 21:06:58 +0800

    Implementation definition or controllers/transaction.js

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -5,8 +5,41 @@ let clientSecret = "EAmLMHHfryzaeiOQZ1WiB6Kq6AKwZxX_SvzwFYGzT0h5ql3Q9KAsyxn0kQZg
 let environment = new paypal.core.SandboxEnvironment(clientId, clientSecret);
 let client = new paypal.core.PayPalHttpClient(environment);
 
+
+let captureOrder =  async function(orderId) {
+	console.log("orderId: " + orderId);
+  request = new paypal.orders.OrdersCaptureRequest(orderId);
+
+  let captureResponse;
+
+  try {
+		captureResponse = await client.execute(request);
+	  let captureResult = captureResponse.result;
+	  let txnId = '';
+
+	  console.log(`${JSON.stringify(captureResult)}`);
+
+	  for(var i=0; i<captureResult.purchase_units.length; i++){
+			for(var j=0; j<captureResult.purchase_units[i].payments.captures.length; j++){
+
+		    if ( captureResult.purchase_units[i].payments.captures[j].final_capture === true ) {
+		      txnId = captureResult.purchase_units[i].payments.captures[j].id;
+		    }
+		  }
+	  }
+
+	  return { txnId: txnId };
+
+	} catch ( err ) {
+		console.log('catch err');
+		console.log(err);
+		return err;
+	}
+}
+
+
 exports.capture = function(req, res) {
 
-
+	let captureResponse = captureOrder(req.body.orderID);
 
 };
