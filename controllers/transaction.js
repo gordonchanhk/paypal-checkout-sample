3ea6f7ca40d374bc49252c3d6d671fc39d70bc8a
Gordon Chan <me@gordon-chan.net>
2020-06-08 01:53:23 +0800

    Reindent line

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -9,38 +9,38 @@ let client = new paypal.core.PayPalHttpClient(environment);
 let captureOrder =  async function(orderId, mockError) {
 	console.log("orderId: " + orderId);
 
-  let captureResponse;
+	let captureResponse;
 
-  //request = new paypal.orders.OrdersCaptureRequest(orderId);
-  var captureReq = {
-	  path: '/v2/checkout/orders/' + orderId + '/capture?',
-	  verb: 'POST',
-	  body: { intent: 'INVALID' },
-	  headers:
-	   { 'Content-Type': 'application/json' }
+	//request = new paypal.orders.OrdersCaptureRequest(orderId);
+	var captureReq = {
+		path: '/v2/checkout/orders/' + orderId + '/capture?',
+		verb: 'POST',
+		body: { intent: 'INVALID' },
+		headers:
+		{ 'Content-Type': 'application/json' }
 	};
 
 	if ( mockError === 'true' ) {
-  	captureReq.headers['PayPal-Mock-Response'] = '{"mock_application_codes":"INSTRUMENT_DECLINED"}';
-  }
+		captureReq.headers['PayPal-Mock-Response'] = '{"mock_application_codes":"INSTRUMENT_DECLINED"}';
+	}
 
-  try {
+	try {
 		captureResponse = await client.execute(captureReq);
-	  let captureResult = captureResponse.result;
-	  let txnId = '';
+		let captureResult = captureResponse.result;
+		let txnId = '';
 
-	  console.log(`${JSON.stringify(captureResult)}`);
+		console.log(`${JSON.stringify(captureResult)}`);
 
-	  for(var i=0; i<captureResult.purchase_units.length; i++){
+		for(var i=0; i<captureResult.purchase_units.length; i++){
 			for(var j=0; j<captureResult.purchase_units[i].payments.captures.length; j++){
 
-		    if ( captureResult.purchase_units[i].payments.captures[j].final_capture === true ) {
-		      txnId = captureResult.purchase_units[i].payments.captures[j].id;
-		    }
-		  }
-	  }
+				if ( captureResult.purchase_units[i].payments.captures[j].final_capture === true ) {
+					txnId = captureResult.purchase_units[i].payments.captures[j].id;
+				}
+			}
+		}
 
-	  return { txnId: txnId };
+		return { txnId: txnId };
 
 	} catch ( err ) {
 		console.log('catch err');
@@ -57,15 +57,15 @@ exports.capture = function(req, res) {
 	let txnId = '';
 
 	captureResponse
-		.then(function(data) {
+	.then(function(data) {
 
-			if ( typeof data.txnId === 'undefined' ) {
-				res.status(400);
-				return res.end( data.message );
-			} else {
-				res.end(JSON.stringify({ txnId: data.txnId }));
-			}
+		if ( typeof data.txnId === 'undefined' ) {
+			res.status(400);
+			return res.end( data.message );
+		} else {
+			res.end(JSON.stringify({ txnId: data.txnId }));
+		}
 
-		});
+	});
 
 };
